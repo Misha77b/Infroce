b427e471e9494abadf20a5e699e329393c83b0f5
Misha <misha77b@gmail.com>
2024-11-22 17:40:27 +0200

    warnings fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,36 +2,33 @@ import React from "react";
 import { Box, AppBar, Toolbar, Typography } from "@mui/material";
 
 const Header = () => {
-  return (
-    <Box>
-      <AppBar
-        position="static"
-        sx={{
-          marginBottom: "30px",
-          background: "transparent",
-        }}
-      >
-        <Toolbar
-          sx={{
-            display: "felx",
-            justifyContent: "center",
-          }}
-        >
-          <Typography
-            variant="h6"
-            sx={{
-              color: "black",
-              fontFamily: "Autour One",
-              fontSize: "24px",
-              fontWeight: "700",
-            }}
-          >
-            Inforce
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+	return (
+		<Box>
+			<AppBar
+				position="static"
+				sx={{
+					marginBottom: "30px",
+					background: "transparent",
+				}}>
+				<Toolbar
+					sx={{
+						display: "felx",
+						justifyContent: "center",
+					}}>
+					<Typography
+						variant="h6"
+						sx={{
+							color: "black",
+							fontFamily: "Autour One",
+							fontSize: "24px",
+							fontWeight: "700",
+						}}>
+						Inforce
+					</Typography>
+				</Toolbar>
+			</AppBar>
+		</Box>
+	);
 };
 
 export default Header;
--- src/router/RootRouter.jsx
+++ src/router/RootRouter.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from "../components/header/Header.jsx";
 import Main from "../components/main/Main.jsx";
+import Header from "../components/header/Header.jsx";
 
 const RootRouter = () => {
 	return (
