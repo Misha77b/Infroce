5f2467814c72031aa6fefcdf53865ddd9fdac5be
Misha <misha77b@gmail.com>
2024-11-22 13:54:31 +0200

    Seaching for problem to fix

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -10,54 +10,58 @@ import PaginationNav from "../pagination/PaginationNav";
 import Search from "../search/Search";
 
 const Main = () => {
-  const dispatch = useDispatch();
-  const [search, setSearch] = useSearchParams();
-  const { params } = useLocationParams();
+	const dispatch = useDispatch();
+	const [search, setSearch] = useSearchParams();
+	const { params } = useLocationParams();
 
-  // pagination
-  const [pageNumber, setPageNumber] = useState(1);
-  const currentPage = search.get("_page");
+	// pagination
+	const [pageNumber, setPageNumber] = useState(1);
+	const currentPage = search.get("_page");
 
-  const loading = useSelector((state) => state.postsReducer.loader);
-  const posts = useSelector((state) => state.postsReducer.posts);
-  console.log(`Fetch posts data:`, posts);
+	const loading = useSelector((state) => state.postsReducer.loader);
+	const posts = useSelector((state) => state.postsReducer.posts);
+	console.log(posts);
+	console.log(`Fetch posts data:`, posts);
 
-  useEffect(() => {
-    if (currentPage === null) {
-      setPageNumber(currentPage ? parseInt(currentPage) : 1);
-    } else {
-      setPageNumber(currentPage ? parseInt(currentPage) : 1);
-    }
-  }, [currentPage]);
+	useEffect(() => {
+		if (currentPage !== String(pageNumber)) {
+			setSearch({ _page: pageNumber });
+		}
+		// if (currentPage === null) {
+		// 	setPageNumber(currentPage ? parseInt(currentPage) : 1);
+		// } else {
+		// 	setPageNumber(currentPage ? parseInt(currentPage) : 1);
+		// }
+		// }, [currentPage]);
+	}, [pageNumber, currentPage, setSearch]);
 
-  useEffect(() => {
-    dispatch(fetchPosts({ params }));
-    window.scrollTo({ top: 0 });
-  }, [pageNumber]);
+	useEffect(() => {
+		dispatch(fetchPosts({ params }));
+		window.scrollTo({ top: 0 });
+	}, [pageNumber]);
 
-  return (
-    <Box
-      className="main"
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      <Search />
-      {loading ? (
-        <Loader />
-      ) : (
-        <>
-          <PostsList postsdata={posts} />
-          <PaginationNav
-            pageNumber={pageNumber}
-            setPageNumber={setPageNumber}
-          />
-        </>
-      )}
-    </Box>
-  );
+	return (
+		<Box
+			className="main"
+			sx={{
+				display: "flex",
+				flexDirection: "column",
+				alignItems: "center",
+			}}>
+			<Search />
+			{loading ? (
+				<Loader />
+			) : (
+				<>
+					<PostsList postsdata={posts} />
+					<PaginationNav
+						pageNumber={pageNumber}
+						setPageNumber={setPageNumber}
+					/>
+				</>
+			)}
+		</Box>
+	);
 };
 
 export default Main;
