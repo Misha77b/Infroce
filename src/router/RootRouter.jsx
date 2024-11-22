b58c1486dcccdb9cd4bd532cfa9538f7c8cb0b0f
Misha <misha77b@gmail.com>
2024-11-22 17:24:58 +0200

    Pagination fixed

--- src/router/RootRouter.jsx
+++ src/router/RootRouter.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import Header from "../components/header/Header";
-import Main from "../components/main/Main";
+import Header from "../components/header/Header.jsx";
+import Main from "../components/main/Main.jsx";
 
 const RootRouter = () => {
-  return (
-    <>
-      <Header />
-      <Main />
-    </>
-  );
+	return (
+		<>
+			<Header />
+			<Main />
+		</>
+	);
 };
 
 export default RootRouter;
